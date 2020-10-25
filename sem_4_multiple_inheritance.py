6bf57bfe61dae8d945f7303d31786b81c38221da
Ivan <3hitemouse@gmail.com>
2020-10-26 01:21:40 +0200

    added basic template

--- sem_4_multiple_inheritance.py
+++ sem_4_multiple_inheritance.py
@@ -1,6 +1,9 @@
 import aiohttp
+import aiohttp_jinja2
 import asyncio
+import jinja2
 import logging
+import os.path
 from aiohttp import web
 
 
@@ -14,13 +17,26 @@ class Base(web.View):
             assert resp.status == 200
             return await resp.text()
 
+    # async def get(self):
+    #     return web.json_response({
+    #         'list': [],
+    #     }, status=200)
+
 
 class Home(Base):
 
     async def get(self):
-        return web.json_response({
-            'list': [],
-        }, status=200)
+        data = {
+            'data': [{
+                'name': 'Ivan Petrov',
+                'group': 'MI-192',
+                'grade': '7.9',
+                'status': 'public',
+            }],
+            'count': 1,
+        }
+        response = aiohttp_jinja2.render_template('home.html', self.request, context=data)
+        return response
 
 
 if __name__ == '__main__':
@@ -32,6 +48,8 @@ if __name__ == '__main__':
 
     app.router.add_view(r'/', handler=Home)
 
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(os.path.abspath('templates')))
+
     logging.getLogger('aiohttp.server').setLevel(logging.DEBUG)
     logging.getLogger('aiohttp.web').setLevel(logging.DEBUG)
 
@@ -39,8 +57,7 @@ if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
 
-    logger.info('======== Running aiohttp (%s) ========\n',
-                aiohttp.__version__, _PORT)
+    logger.info('======== Running aiohttp (%s) ========\n', aiohttp.__version__)
 
     web.run_app(
         app, host='localhost', port=_PORT,
