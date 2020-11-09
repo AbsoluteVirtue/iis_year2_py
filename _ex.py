158e9aec0958d85854ecba31b493f479793ce4f8
Ivan <3hitemouse@gmail.com>
2020-11-09 15:40:10 +0200

    new sem

--- _ex.py
+++ _ex.py
@@ -1,20 +1,29 @@
-import aiohttp
-import aiohttp_jinja2
-import asyncio
-import jinja2
-import os
-from aiohttp import web
-
-
-class List:
-    pass
+import requests
 
 
 if __name__ == '__main__':
-    app = web.Application(loop=asyncio.get_event_loop())
+    result = []
+    for i in range(1, int(12000/12)):
+        try:
+            res = requests.get((
+                'https://www.kickstarter.com/discover/advanced'
+                '?state=successful&category_id=34&sort=most_backed&format=json'
+                '&page=%s' % i))
+            data = res.json()
+        except Exception as ex:
+            pass
+        else:
+            for project in data.get('projects', []):
+                result.append({
+                    'name': project.get('name', '-'),
+                    'description': project.get('blurb', '-'),
+                    'url': project.get('urls', {}).get('web', {}).get('project', '-'),
+                    'amount': project.get('pledged', 0.0),
+                    'currency': project.get('currency', '-'),
+                    'author': project.get('creator', {}).get('name', '-'),
+                    'backers': project.get('backers_count', 0),
+                    'date': project.get('launched_at', 0),
+                    '_id': project['id'],
+                })
 
-    app['client'] = aiohttp.ClientSession()
-    app.router.add_view(r'/', handler=List)
-
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(os.path.abspath('templates')))
-    web.run_app(app, host='localhost', port=9980)
+    print(result)
