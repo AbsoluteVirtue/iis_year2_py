ae254a59f318dbfe65d8b97019508857c30e4230
Ivan <3hitemouse@gmail.com>
2020-12-16 00:26:07 +0200

    small tweaks to eval script

--- eval.py
+++ eval.py
@@ -1,6 +1,9 @@
 import students
 
 
+NUMBER_OF_GRADES_TO_CONSIDER = 4
+
+
 def calc(first, second, attendance, *grades):
     """
         (eval 1 * 0.15) + (eval 2 * 0.15) + (median labs * 0.15) + (practice * 0.15) = 60% of final grade
@@ -29,7 +32,7 @@ def calc(first, second, attendance, *grades):
         },
     }
 
-    second = median(*max_(3, grades))
+    second = median(*max_(NUMBER_OF_GRADES_TO_CONSIDER, grades))
     total = median(first, second, attendance, labs)
 
     result['soft'] = {
@@ -43,18 +46,17 @@ def calc(first, second, attendance, *grades):
 
 
 if __name__ == '__main__':
-    for student in students.YEAR_ONE_HARD:
-        _name, _group, _first, _second, _attendance, *grades = student
+    for _name, _group, _first, _second, _attendance, *_grades in students.YEAR_ONE_EASY:
         # first, second, attendance, *grades
-        res = calc(_first, _second, _attendance, *grades)
+        _res = calc(_first, _second, _attendance, *_grades)
         print(f'{_name} ({_group})')
-        print((f'lab: {res["hard"]["labs"]:.2f}, '
+        print((f'\tlab: {_res["hard"]["labs"]:.2f}, '
                f'att 1: {_first:.2f}, '
                f'att 2: {_second:.2f}, '
                f'sem: {_attendance:.2f} = '
-               f'{res["hard"]["total"]:.2f} ({res["hard"]["final"]:.2f}|{res["hard"]["final"] + 40:.2f})'))
-        print((f'lab: {res["soft"]["labs"]:.2f}, '
+               f'{_res["hard"]["total"]:.2f} ({_res["hard"]["final"]:.2f}|{_res["hard"]["final"] + 40:.2f})'))
+        print((f'\tlab: {_res["soft"]["labs"]:.2f}, '
                f'att 1: {_first:.2f}, '
-               f'att 2: {res["soft"]["recalc_second"]:.2f}, '
+               f'att 2: {_res["soft"]["recalc_second"]:.2f}, '
                f'sem: {_attendance:.2f} = '
-               f'{res["soft"]["total"]:.2f} ({res["soft"]["final"]:.2f}|{res["soft"]["final"] + 40:.2f})'))
+               f'{_res["soft"]["total"]:.2f} ({_res["soft"]["final"]:.2f}|{_res["soft"]["final"] + 40:.2f})'))
